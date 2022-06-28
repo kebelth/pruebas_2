fffc99c37a340f771f4e558f0deb067a127be03b
kebelth <kebelth@DESKTOP-H1NEUSK>
2022-06-27 23:36:18 -0600

    semana3 pruebas

---
+++ src/test/java/prueba/ver3/Prueba3ApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.ver3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
